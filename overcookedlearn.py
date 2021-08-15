6a36f92a8f5612fa5952bbe01969540cb8d3a245
Andy Shih <andyshih12@gmail.com>
2021-08-14 22:59:39 -0700

    fix typo from last commit

--- overcookedlearn.py
+++ overcookedlearn.py
@@ -9,7 +9,7 @@ from multiagentworld.common.wrappers import SimultaneousFrameStack
 
 env = SimultaneousFrameStack(gym.make("OvercookedMultiEnv-v0", layout_name="random0"), numframes=3)
 env.add_partner_agent(OnPolicyAgent(PPO("MlpPolicy", env)))
-# env.and_partner_agent(OffPolicyAgent(DQN("MlpPolicy", env)))
+# env.add_partner_agent(OffPolicyAgent(DQN("MlpPolicy", env)))
 
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=100000)
