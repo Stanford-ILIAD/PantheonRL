5ad0ac848d6c888da1319b214fd89cfbfda5c724
Aditi T <31555219+ololiti@users.noreply.github.com>
2021-08-19 10:33:10 -0700

    welcome page for website

---
+++ website/__init__.py
@@ -0,0 +1,27 @@
+# create the web app
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'multiagentworld.sqlite')
+    )
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from website import welcome
+    app.register_blueprint(welcome.bp)
+
+    return app
