0b4f02886715b3369a2d092ee87e67b70fdf7bb9
Andy Shih <andyshih12@gmail.com>
2021-09-13 17:12:43 -0700

    touch up on website visuals

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,13 @@
 # create the web app
 import os
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    Bootstrap(app)
+    
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'multiagentworld.sqlite')
